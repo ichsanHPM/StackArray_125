5c23b9767ab0b5322143fc0d3da5f4a03d31cf22
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-06-15 18:13:30 +0700

    constructor

--- StackArray_125/StackArray_125.cpp
+++ StackArray_125/StackArray_125.cpp
@@ -2,6 +2,19 @@
 #include <string>
 using namespace std;
 
+class stackArray {
+private:
+    string stack_array[5];
+    int top;
+
+public:
+    //constructor
+    stackArray() {
+        top = -1;
+    }
+
+    
+};
 
 int main()
 {
