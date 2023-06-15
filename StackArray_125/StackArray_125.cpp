a81b00779340790bf863fa929bab1ccf9aec1957
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-06-15 18:28:13 +0700

    else

--- StackArray_125/StackArray_125.cpp
+++ StackArray_125/StackArray_125.cpp
@@ -48,6 +48,11 @@ public:
         if (empty()) {
             cout << "\nStack is empty." << endl;
         }
+        else {
+            for (int tmp = 0; tmp <= top; tmp++) {
+                cout << stack_array[tmp] << endl;
+            }
+        }
     }
 };
 
